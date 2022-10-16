d5c7783a09527178b77672a4b60b6385c32c7554
Sanchit Kedia <61658557+Sanchitkedia@users.noreply.github.com>
2022-10-16 00:18:00 -0400

    Update CodeCoverage.cmake

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -162,9 +162,9 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 
 	# Show info where to find the report
 	#ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
-		COMMAND ;
-		COMMENT "Open ./${_outputname}/index.html in your browser to view the coverage report."
-	)
+		#COMMAND ;
+		#COMMENT "Open ./${_outputname}/index.html in your browser to view the coverage report."
+	#)
 
 ENDFUNCTION() # SETUP_TARGET_FOR_COVERAGE
 
