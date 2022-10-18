02061136a26e269f0cc8d1bd9e8c33303aa43ee8
Tanmay Haldankar <tanmay.n.haldankar99@gmail.com>
2022-10-18 10:48:56 -0400

    Implementing forward_kinematics.cpp with stubs

--- app/forward_kinematics.cpp
+++ app/forward_kinematics.cpp
@@ -5,34 +5,19 @@
 #include "../include/forward_kinematics.hpp"
 #include "../include/Eigen/Dense"
 
-using Eigen::MatrixXd;
+using Eigen::Matrix;
 using namespace std;
 
-Eigen::Matrix<double, 4, 4> ForwardKinematics::calculate_TF(int j){
-    
-    int i = j;
-    Eigen::Matrix<double, 4, 4> Tf{
-        {cos(_dh_matrix(i, 0)), -sin(_dh_matrix(i, 0)) * cos(_dh_matrix(i, 1)), sin(_dh_matrix(i,0)) * sin(_dh_matrix(i,1)), _dh_matrix(i, 2) * cos(_dh_matrix(i, 0))},
-        {sin(_dh_matrix(i, 0)), cos(_dh_matrix(i, 0)) * cos(_dh_matrix(i, 1)), -cos(_dh_matrix(i,0)) * sin(_dh_matrix(i,1)), _dh_matrix(i, 2) * sin(_dh_matrix(i, 0))},
-        {0, sin(_dh_matrix(i, 1)), cos(_dh_matrix(i, 1)), _dh_matrix(i,3)},
-        {0, 0 , 0, 1},
-    };
+Matrix<double, 4, 4> ForwardKinematics::calculate_TF(int j){
+    cout << j << endl;
+    ForwardKinematics k;
+    _dh_matrix = k.get_dh_parameters();
+    Matrix<double, 4, 4>Tf = Matrix<double, 4, 4>::Identity();   
     return Tf;
 }
 
-Eigen::Matrix<double, 4, 4> ForwardKinematics::solve_fk(){
+Matrix<double, 4, 4> ForwardKinematics::solve_fk(){
 
-    _dh_matrix = RobotParameters::get_dh_parameters();
-
-    Eigen::Matrix<double, 4, 4>Tf1 = Eigen::Matrix<double, 4, 4>::Identity();
-
-
-
-    for(int i = 0; i < 6; i++)
-    {   
-        Eigen::Matrix<double, 4, 4> Tf_i = calculate_TF(i);
-        Tf1 = Tf1 * Tf_i;  
-    }
-
+    Matrix<double, 4, 4>Tf1 = Matrix<double, 4, 4>::Identity();
     return Tf1;
 }
