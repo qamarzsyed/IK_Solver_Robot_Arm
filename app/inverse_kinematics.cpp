b589f0ed10db078c25a60d864d50ca5ca7229773
Sanchit Kedia <sanchit@terpmail.umd.edu>
2022-10-26 00:15:39 -0400

    Debugging, few bugs fixed but new bugs found

--- app/inverse_kinematics.cpp
+++ app/inverse_kinematics.cpp
@@ -9,15 +9,19 @@
  *
  */
 #include "../include/inverse_kinematics.hpp"
+#include "robot_parameters.hpp"
+#include <cmath>
+#include <math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using std::cout;
 using std::endl;
+using std::sqrt;
 using std::vector;
 
 InverseKinematics::InverseKinematics() {
-  _eff_angles = {0, 0, 0};
+  _eff_angles = {-M_PI_2, 0, -M_PI_2};
   _eff_position = {0, 0.45, 0.695};
 }
 
@@ -35,11 +39,13 @@ void InverseKinematics::set_eff_position(vector<double> eff_position) {
 
 vector<double> InverseKinematics::solve_ik() {
   ForwardKinematics f;
-  MatrixXd dh = get_dh_parameters();
+  RobotParameters r;
+  VectorXd dh_a = get_dh_parameters().col(2);
+  VectorXd dh_d = get_dh_parameters().col(3);
 
   double x_03 = 0.00;
   double y_03 = 0.08;
-  double z_03 = 0.09;
+  double z_03 = 0.00;
 
   double x_0e = _eff_position[0];
   double y_0e = _eff_position[1];
@@ -50,19 +56,22 @@ vector<double> InverseKinematics::solve_ik() {
   double z_3e = z_0e - z_03;
 
   double theta_1 = atan2(y_3e, x_3e);
+  double r1 = sqrt(pow(x_3e, 2) + pow(y_3e, 2));
+  double r2 = z_3e - dh_d[0];
+  double phi_2 = atan2(r2, r1);
+  double r3 = sqrt(pow(r1, 2) + pow(r2, 2));
+  double phi_3 = (pow(r3, 2) - pow(dh_a[1], 2) - pow(dh_d[3], 2)) /
+                      (2 * dh_a[1] * dh_d[3]);
+  double theta_3 = atan2(phi_3,sqrt(1-pow(phi_3,2)));
+  double theta_2 = phi_2 - atan2(dh_a[1]+dh_d[2]*cos(theta_3),dh_d[2]*sin(theta_3));
 
-  double r_3e = sqrt(pow(x_3e, 2) + pow(y_3e, 2) + pow(z_3e - dh.coeff(0,3), 2));
-
-  double theta_3 = acos((- pow(dh.coeff(1,2),2) - pow(dh.coeff(3,2), 2) + pow(r_3e, 2)) /
-                        (2 * dh.coeff(1,2) * dh.coeff(3,2)));
-
-  double theta_2 =
-      atan2(z_3e-dh.coeff(0,3), sqrt(pow(x_3e, 2) + pow(y_3e, 2))) -
-      atan2(dh.coeff(3,2) * sin(theta_3), dh.coeff(1,2) + dh.coeff(3,2) * cos(theta_3));
 
   MatrixXd Tf_i(4, 4);
   MatrixXd Tf1(4, 4);
   Tf1 = MatrixXd::Identity(4, 4);
+  vector<double> robot_angles = {theta_1, theta_2, theta_3, 0, 0, 0};
+  r.set_robot_angles(robot_angles);
+
   for (int i = 0; i < 3; i++) {
     Tf_i = f.calculate_TF(i);
     Tf1 = Tf1 * Tf_i;
@@ -70,36 +79,42 @@ vector<double> InverseKinematics::solve_ik() {
   MatrixXd Rotation = Tf1.block<3, 3>(0, 0);
 
   MatrixXd R06(3, 3);
-  R06(0, 0) = cos(_eff_angles[2]) * cos(_eff_angles[1]);
-  R06(0, 1) = -sin(_eff_angles[2]) * cos(_eff_angles[0]) +
-              cos(_eff_angles[2]) * sin(_eff_angles[1]) * sin(_eff_angles[0]);
-  R06(0, 2) = sin(_eff_angles[2]) * sin(_eff_angles[0]) +
-              cos(_eff_angles[2]) * sin(_eff_angles[1]) * cos(_eff_angles[0]);
-  R06(1, 0) = sin(_eff_angles[2]) * cos(_eff_angles[1]);
-  R06(1, 1) = cos(_eff_angles[2]) * cos(_eff_angles[0]) +
+  R06(0, 0) = cos(_eff_angles[2]) * cos(_eff_angles[0]) -
               sin(_eff_angles[2]) * sin(_eff_angles[1]) * sin(_eff_angles[0]);
-  R06(1, 2) = -cos(_eff_angles[2]) * sin(_eff_angles[0]) +
+  R06(0, 1) = -cos(_eff_angles[1]) * sin(_eff_angles[0]);
+  R06(0, 2) = cos(_eff_angles[2]) * sin(_eff_angles[0]) +
               sin(_eff_angles[2]) * sin(_eff_angles[1]) * cos(_eff_angles[0]);
-  R06(2, 0) = -sin(_eff_angles[1]);
-  R06(2, 1) = cos(_eff_angles[1]) * sin(_eff_angles[0]);
-  R06(2, 2) = cos(_eff_angles[1]) * cos(_eff_angles[0]);
+
+  R06(2, 0) = -sin(_eff_angles[2]) * cos(_eff_angles[1]);
+  R06(2, 1) = sin(_eff_angles[1]);
+  R06(2, 2) = cos(_eff_angles[2]) * cos(_eff_angles[1]);
+
+  R06(1, 2) = sin(_eff_angles[2]) * sin(_eff_angles[0]) -
+              cos(_eff_angles[2]) * sin(_eff_angles[1]) * cos(_eff_angles[0]);
+  R06(1, 1) = cos(_eff_angles[1]) * cos(_eff_angles[0]);
+  R06(1, 0) = sin(_eff_angles[2]) * cos(_eff_angles[0]) +
+              cos(_eff_angles[2]) * sin(_eff_angles[1]) * sin(_eff_angles[0]);
 
   MatrixXd R36(3, 3);
-  R36 = Rotation.inverse() * R06;
+  R36 = Rotation.transpose() * R06;
 
-  cout << "R06\n" << R06 << endl;
+  double theta_5 =
+      atan2(sin(theta_1) * R06.coeff(0, 2) - cos(theta_1) * R06.coeff(1, 2),
+            sqrt(1 - pow(sin(theta_1) * R06.coeff(0, 2) -
+                             cos(theta_1) * R06.coeff(1, 2),
+                         2)));
+  double theta_6 =
+      atan2(-sin(theta_1) * R06.coeff(0, 0) + cos(theta_1) * R06.coeff(1, 0),
+            sin(theta_1) * R06.coeff(0, 1) - cos(theta_1) * R06.coeff(1, 1));
+  double theta_4 =
+      atan2(cos(theta_1) * cos(theta_2 + theta_3) * R06.coeff(0, 2) +
+                sin(theta_1) * cos(theta_2 + theta_3) * R06.coeff(1, 2) +
+                sin(theta_2 + theta_3) * R06.coeff(2, 2),
+            -cos(theta_1) * sin(theta_2 + theta_3) * R06.coeff(0, 2) -
+                sin(theta_1) * sin(theta_2 + theta_3) * R06.coeff(1, 2) +
+                cos(theta_2 + theta_3) * R06.coeff(2, 2));
 
-  double theta_5 = acos(-R06.coeff(2, 2));
-  double theta_6 = acos(-R06.coeff(2, 0) / sin(theta_5));
-  double theta_4 = asin(-R06.coeff(1, 2) / sin(theta_5));
-
-  vector<double> robot_angles = {theta_1, theta_2, theta_3,
-                                 theta_4, theta_5, theta_6};
-
-  // cout << "Angles I" << endl;
-  // for (int i = 0; i < 6; i++) {
-  //  cout << robot.coeff()es[i] << endl;
-  //}
+  robot_angles = {theta_1, theta_2, theta_3, theta_4, theta_5, theta_6};
 
   cout << "Theta 1 is " << theta_1 << endl;
   cout << "Theta 2 is " << theta_2 << endl;
@@ -109,4 +124,3 @@ vector<double> InverseKinematics::solve_ik() {
   cout << "Theta 6 is " << theta_6 << endl;
 
   return robot_angles;
-}
