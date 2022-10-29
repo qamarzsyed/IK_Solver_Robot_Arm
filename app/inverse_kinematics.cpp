9af21af96b0601380ca1ee42c54fdd0697e7fc49
Sanchit Kedia <sanchit@terpmail.umd.edu>
2022-10-28 23:58:36 -0400

    Fixing program bugs

--- app/inverse_kinematics.cpp
+++ app/inverse_kinematics.cpp
@@ -9,118 +9,83 @@
  *
  */
 #include "../include/inverse_kinematics.hpp"
-#include "robot_parameters.hpp"
-#include <cmath>
-#include <math.h>
 
-using Eigen::MatrixXd;
+using Eigen::Matrix2d;
+using Eigen::Matrix3d;
+using Eigen::Matrix4d;
 using Eigen::VectorXd;
-using std::cout;
-using std::endl;
+using Eigen::Vector3d;
 using std::sqrt;
 using std::vector;
 
 InverseKinematics::InverseKinematics() {
-  _eff_angles = {-M_PI_2, 0, -M_PI_2};
-  _eff_position = {0, 0.45, 0.695};
+  Matrix3d E{
+      {-0.1268, -0.9427, -0.3085},
+      {0.9268, -0.0017, -0.3756},
+      {0.3535, -0.3336, 0.8739},
+  };
+  R_E = E;
+  _eff_position = {-0.2283, 0.0216, 0.4788};
 }
 
-vector<double> InverseKinematics::get_eff_angles() { return _eff_angles; }
+void InverseKinematics::set_eff_rotation(Eigen::Matrix3d _R_E) { R_E = _R_E; }
 
-vector<double> InverseKinematics::get_eff_position() { return _eff_position; }
+Eigen::Matrix3d InverseKinematics::get_eff_rotation() { return R_E; }
 
-void InverseKinematics::set_eff_angles(vector<double> eff_angles) {
-  _eff_angles = eff_angles;
-}
+Eigen::Vector3d InverseKinematics::get_eff_position() { return _eff_position; }
 
-void InverseKinematics::set_eff_position(vector<double> eff_position) {
+void InverseKinematics::set_eff_position(Eigen::Vector3d eff_position) {
   _eff_position = eff_position;
 }
 
 vector<double> InverseKinematics::solve_ik() {
   ForwardKinematics f;
   RobotParameters r;
+
   VectorXd dh_a = get_dh_parameters().col(2);
   VectorXd dh_d = get_dh_parameters().col(3);
 
-  double x_03 = 0.00;
-  double y_03 = 0.08;
-  double z_03 = 0.00;
+  VectorXd c_p = _eff_position - dh_d[5] * R_E.col(2);
 
-  double x_0e = _eff_position[0];
-  double y_0e = _eff_position[1];
-  double z_0e = _eff_position[2];
+  double d3 = sqrt(pow(c_p[0], 2) + pow(c_p[1], 2) +
+                   pow((c_p[2] - dh_d[0]), 2) - pow(dh_d[1], 2));
 
-  double x_3e = x_0e - x_03;
-  double y_3e = y_0e - y_03;
-  double z_3e = z_0e - z_03;
+  double D = (c_p[2] - dh_d[0]) / d3;
+  double theta_2 = atan2(D, sqrt(1 - pow(D, 2)));
 
-  double theta_1 = atan2(y_3e, x_3e);
-  double r1 = sqrt(pow(x_3e, 2) + pow(y_3e, 2));
-  double r2 = z_3e - dh_d[0];
-  double phi_2 = atan2(r2, r1);
-  double r3 = sqrt(pow(r1, 2) + pow(r2, 2));
-  double phi_3 = (pow(r3, 2) - pow(dh_a[1], 2) - pow(dh_d[3], 2)) /
-                      (2 * dh_a[1] * dh_d[3]);
-  double theta_3 = atan2(phi_3,sqrt(1-pow(phi_3,2)));
-  double theta_2 = phi_2 - atan2(dh_a[1]+dh_d[2]*cos(theta_3),dh_d[2]*sin(theta_3));
+  Matrix2d M{
+      {-dh_d[1], -dh_d[2] * cos(theta_2)},
+      {-dh_d[2] * cos(theta_2), dh_d[1]},
+  };
+  Eigen::Vector2d R{c_p[0], c_p[1]};
+  VectorXd sol = M.inverse() * R;
+  double theta_1 = atan2(sol[0], sol[1]);
 
+  double theta_3 = -1.5708;
 
-  MatrixXd Tf_i(4, 4);
-  MatrixXd Tf1(4, 4);
-  Tf1 = MatrixXd::Identity(4, 4);
-  vector<double> robot_angles = {theta_1, theta_2, theta_3, 0, 0, 0};
-  r.set_robot_angles(robot_angles);
+  vector<double> I_Theta = {theta_1, theta_2 - 1.5708, theta_3, 0, 0, 0};
 
+  set_robot_angles(I_Theta);
+  set_dh_parameters();
+
+  Matrix4d H0_3 = Matrix4d::Identity();
   for (int i = 0; i < 3; i++) {
-    Tf_i = f.calculate_TF(i);
-    Tf1 = Tf1 * Tf_i;
+    Matrix4d Tf_ = f.calculate_TF(i, get_dh_parameters());
+    H0_3 = H0_3 * Tf_;
   }
-  MatrixXd Rotation = Tf1.block<3, 3>(0, 0);
+  Matrix3d R0_3 = H0_3.block<3, 3>(0, 0);
 
-  MatrixXd R06(3, 3);
-  R06(0, 0) = cos(_eff_angles[2]) * cos(_eff_angles[0]) -
-              sin(_eff_angles[2]) * sin(_eff_angles[1]) * sin(_eff_angles[0]);
-  R06(0, 1) = -cos(_eff_angles[1]) * sin(_eff_angles[0]);
-  R06(0, 2) = cos(_eff_angles[2]) * sin(_eff_angles[0]) +
-              sin(_eff_angles[2]) * sin(_eff_angles[1]) * cos(_eff_angles[0]);
+  Matrix3d R3_6 = R0_3.transpose() * R_E;
 
-  R06(2, 0) = -sin(_eff_angles[2]) * cos(_eff_angles[1]);
-  R06(2, 1) = sin(_eff_angles[1]);
-  R06(2, 2) = cos(_eff_angles[2]) * cos(_eff_angles[1]);
-
-  R06(1, 2) = sin(_eff_angles[2]) * sin(_eff_angles[0]) -
-              cos(_eff_angles[2]) * sin(_eff_angles[1]) * cos(_eff_angles[0]);
-  R06(1, 1) = cos(_eff_angles[1]) * cos(_eff_angles[0]);
-  R06(1, 0) = sin(_eff_angles[2]) * cos(_eff_angles[0]) +
-              cos(_eff_angles[2]) * sin(_eff_angles[1]) * sin(_eff_angles[0]);
-
-  MatrixXd R36(3, 3);
-  R36 = Rotation.transpose() * R06;
+  double theta_4 = atan2(R3_6(1, 2), R3_6(0, 2));
 
   double theta_5 =
-      atan2(sin(theta_1) * R06.coeff(0, 2) - cos(theta_1) * R06.coeff(1, 2),
-            sqrt(1 - pow(sin(theta_1) * R06.coeff(0, 2) -
-                             cos(theta_1) * R06.coeff(1, 2),
-                         2)));
-  double theta_6 =
-      atan2(-sin(theta_1) * R06.coeff(0, 0) + cos(theta_1) * R06.coeff(1, 0),
-            sin(theta_1) * R06.coeff(0, 1) - cos(theta_1) * R06.coeff(1, 1));
-  double theta_4 =
-      atan2(cos(theta_1) * cos(theta_2 + theta_3) * R06.coeff(0, 2) +
-                sin(theta_1) * cos(theta_2 + theta_3) * R06.coeff(1, 2) +
-                sin(theta_2 + theta_3) * R06.coeff(2, 2),
-            -cos(theta_1) * sin(theta_2 + theta_3) * R06.coeff(0, 2) -
-                sin(theta_1) * sin(theta_2 + theta_3) * R06.coeff(1, 2) +
-                cos(theta_2 + theta_3) * R06.coeff(2, 2));
+      atan2(sqrt(pow(R3_6(1, 2), 2) + pow(R3_6(0, 2), 2)), R3_6(2, 2));
 
-  robot_angles = {theta_1, theta_2, theta_3, theta_4, theta_5, theta_6};
+  double theta_6 = atan2(R3_6(2, 1), -R3_6(2, 0));
 
-  cout << "Theta 1 is " << theta_1 << endl;
-  cout << "Theta 2 is " << theta_2 << endl;
-  cout << "Theta 3 is " << theta_3 << endl;
-  cout << "Theta 4 is " << theta_4 << endl;
-  cout << "Theta 5 is " << theta_5 << endl;
-  cout << "Theta 6 is " << theta_6 << endl;
+  vector<double> F_Theta = {theta_1, theta_2 - 1.5708, theta_3,
+                            theta_4, theta_5, theta_6};
 
-  return robot_angles;
+  return F_Theta;
+}
